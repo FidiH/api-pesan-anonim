f9f515362f6a62b2eeae4133e24e460c327bfa2a
fidiH <fidihartato.pidi@gmail.com>
2025-04-03 11:31:24 +0700

    merubah file server

--- server.mjs
+++ server.mjs
@@ -12,15 +12,18 @@ const port = process.env.PORT || 3000;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 // Konfigurasi multer untuk Cloudinary
 const upload = multer({
   storage: new CloudinaryStorage({
     cloudinary,
-    params: { folder: "uploads", allowed_formats: ["jpg", "png", "mp4"] },
-  }),
+    params: {
+      folder: "uploads",
+      allowed_formats: ["jpg", "webp", "png", "mp4"]
+    }
+  })
 });
 
 app.use(express.json());
@@ -28,20 +31,34 @@ const GAS_URL = process.env.GAS_URL;
 
 // API Submit Form
 app.post("/upload", upload.single("media"), async (req, res) => {
-  if (!req.body.nama || !req.body.pesan) return res.status(400).json({ error: "Nama & pesan wajib!" });
+  if (!req.body.nama)
+    return res
+      .status(400)
+      .json({ error: "Nama nya jangan dikosongin ya baby" });
+  if (!req.body.pesan)
+    return res
+      .status(400)
+      .json({ error: "Masa iya kirim ginian pesannya kosong sih baby" });
 
-  const data = { nama: req.body.nama, pesan: req.body.pesan, file: req.file?.path || null };
+  const data = {
+    nama: req.body.nama,
+    pesan: req.body.pesan,
+    file: req.file?.path || null
+  };
 
   try {
     const response = await fetch(GAS_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
+      body: JSON.stringify(data)
     });
 
-    res.json({ balasan: "Data berhasil dikirim!", response: await response.json() });
+    res.json({
+      balasan: "Sudah terkikirim, thankyou baby",
+      response: await response.json()
+    });
   } catch {
-    res.status(500).json({ error: "Gagal menyimpan ke Google Sheets" });
+    res.status(500).json({ error: "Gagal menyimpan data ke google sheet" });
   }
 });
 
